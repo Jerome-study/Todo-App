5bb588b271dc93ac2c93f79995b3583c02184e74
Jerome-study <119875460+Jerome-study@users.noreply.github.com>
2023-06-21 22:37:47 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+let textBox = document.getElementById('text-box')
+let addButton = document.querySelector('.add-todo')
+let todoList = document.querySelector('.todo-list')
+let isEmpty = true
+let todos =[
+    
+]
+
+
+
+render ()
+function render () {
+    todoList.innerHTML = ""
+    todos.forEach(todo=> {
+        const element = document.createElement('div')
+        const todoName = document.createElement('p')
+        const deleteButton = document.createElement('button')
+    
+        element.classList.add('todo-holder')
+        deleteButton.classList.add('deleteButton')
+    
+        todoName.innerText = todo.todoName
+        deleteButton.innerText = "X"
+        deleteButton.id = todo.id
+        
+        element.appendChild(todoName)
+        element.appendChild(deleteButton)
+        todoList.appendChild(element)
+    })
+
+    let deleteBtn = document.querySelectorAll('.deleteButton')
+    deleteBtn.forEach(button => {
+        button.addEventListener('click', deleteTodo)
+    })
+}
+
+function deleteTodo() {
+    const todoId = parseInt(this.id)
+
+    checkId(todoId)
+}
+
+function checkId (todoId) {
+  todos =  todos.filter(todo => {
+    if (todo.id === todoId) {
+        return false
+    } else {
+        return true
+    }
+   })
+   render()
+}
+
+
+
+
+addButton.addEventListener('click', addTodo)
+
+
+
+function addTodo() {
+    if (textBox.value === "") {
+        return
+    } else {
+        todos.push({
+            todoName: textBox.value,
+            id: todos.length 
+        })
+        textBox.value = ""
+    }
+    
+   
+   render()
+}
+
+
+
